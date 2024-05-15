2820d69054108b83f6aa59e0a1ada6f36b7f39be
alan walker <razor1895@gmail.com>
2024-05-15 17:08:31 +0800

    chore: let anchor to be option center

--- packages/terre2/assets/templates/WebGAL_Template/assets/initRegister-75615da6.js
+++ packages/terre2/assets/templates/WebGAL_Template/assets/initRegister-75615da6.js
@@ -1,4 +1,4 @@
-import { r as registerPerform, W as WebGAL, C as Container, T as Texture, S as Sprite, a as SCREEN_CONSTANTS } from "./index-bca80167.js";
+import { r as registerPerform, W as WebGAL, C as Container, T as Texture, S as Sprite, a as SCREEN_CONSTANTS } from "./index-4dc0a717.js";
 const pixicherryBlossoms = (cherryBlossomsSpeed) => {
   const scalePreset = 0.15;
   const effectsContainer = WebGAL.gameplay.pixiStage.effectsContainer;
