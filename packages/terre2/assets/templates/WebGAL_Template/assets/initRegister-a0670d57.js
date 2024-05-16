e6551c5ae9b6c5426b3d742bf184ded82de9ec88
alan walker <razor1895@gmail.com>
2024-05-16 16:14:43 +0800

    chore: preload video

--- packages/terre2/assets/templates/WebGAL_Template/assets/initRegister-a0670d57.js
+++ packages/terre2/assets/templates/WebGAL_Template/assets/initRegister-a0670d57.js
@@ -1,4 +1,4 @@
-import { r as registerPerform, W as WebGAL, C as Container, T as Texture, S as Sprite, a as SCREEN_CONSTANTS } from "./index-4dc0a717.js";
+import { r as registerPerform, W as WebGAL, C as Container, T as Texture, S as Sprite, a as SCREEN_CONSTANTS } from "./index-70624a92.js";
 const pixicherryBlossoms = (cherryBlossomsSpeed) => {
   const scalePreset = 0.15;
   const effectsContainer = WebGAL.gameplay.pixiStage.effectsContainer;
