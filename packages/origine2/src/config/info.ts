13dcf088e8ba87e662a2dc2a93d67e1b9c961980
alan walker <razor1895@gmail.com>
2024-06-14 16:59:48 +0800

    chore: bugfix

--- packages/origine2/src/config/info.ts
+++ packages/origine2/src/config/info.ts
@@ -5,5 +5,5 @@ export interface Info {
 
 export const __INFO: Info = {
   version: '4.4.13',
-  buildTime: '2024-06-13T11:53:23.018Z', // 编译时会通过 version-sync.js 自动更新
+  buildTime: '2024-06-13T13:33:21.577Z', // 编译时会通过 version-sync.js 自动更新
 };
