7b95393d8eaff7007eabbfd48ad1d0314f036dda
alan walker <razor1895@gmail.com>
2024-07-16 18:18:10 +0800

    chore: 标题页UI自定义

--- packages/origine2/src/pages/editor/Topbar/tabs/GameConfig/GameConfig.tsx
+++ packages/origine2/src/pages/editor/Topbar/tabs/GameConfig/GameConfig.tsx
@@ -3,7 +3,7 @@ import {useValue} from "../../../../../hooks/useValue";
 import axios from "axios";
 import {useSelector} from "react-redux";
 import {RootState} from "../../../../../store/origineStore";
-import React, {useState, useEffect, useRef} from "react";
+import React, {useState, useEffect, useRef, useMemo} from "react";
 import {cloneDeep} from "lodash";
 import ChooseFile from "../../../ChooseFile/ChooseFile";
 import useTrans from "@/hooks/useTrans";
@@ -15,25 +15,44 @@ import {textboxThemes} from "./constants";
 import {eventBus} from "@/utils/eventBus";
 import {TabItem} from "@/pages/editor/Topbar/components/TabItem";
 import {Add, Plus, Write} from "@icon-park/react";
-import { Button, Dropdown, Input, Option, Checkbox } from "@fluentui/react-components";
+import {
+  Button,
+  Dropdown,
+  Input,
+  Option,
+  Dialog,
+  DialogTrigger,
+  DialogSurface,
+  DialogTitle,
+  DialogContent,
+  DialogBody,
+  DialogActions,
+  Checkbox,
+} from "@fluentui/react-components";
 import { Dismiss24Filled, Dismiss24Regular, bundleIcon } from "@fluentui/react-icons";
 
-interface IMenuConfig {
-  menuName: string
-  key: string
-  show: boolean
+export enum GameMenuKey {
+  Game_start_button = "Game_start_button",
+  Game_achievement_button="Game_achievement_button",
+  Game_storyline_button="Game_storyline_button",
+  Game_extra_button="Game_extra_button",
 }
 
-const boolMap = new Map<string, boolean>([
-  ['true', true],
-  ['false', false]
-])
-
-const menuKeysMap = new Map<string, string>([
-  ['achieve', '成就'],
-  ['storyline', '故事线'],
-  ['beautyGuide', '美女图鉴']
-])
+interface GameMenuItem {
+  content: string,
+  args: {
+    hide: boolean,
+      style: {
+      x?: number;
+      y?: number;
+      scale?: number;
+      image?: string;
+      fontSize?: number;
+      fontColor?: string;
+      countdown?: number;
+    }
+  }
+}
 
 
 export default function GameConfig() {
@@ -66,21 +85,8 @@ export default function GameConfig() {
   }
 
   function getConfigContentAsStringArray(key: string) {
-    console.log(gameConfig.value)
+    console.log(gameConfig.value);
     return gameConfig.value.find(e => e.command === key)?.args ?? [];
-  }
-
-  function getConfigGameMenuArray(key: string) {
-    const args = gameConfig.value.find(e => e.command === key)?.args as unknown as IMenuConfig[] ?? [];
-
-    return args?.map((e: any) => {
-      const arr: any = typeof e === 'string' ? e.split('-') : e;
-      return { 
-        menuName: typeof e === 'string' ? menuKeysMap.get(arr[0]) : e, 
-        key: typeof e === 'string' ?  arr[0] : e,
-        show: typeof e === 'string' ? boolMap.get(arr[1]) : e
-      };
-    }) ?? [];
   }
 
   function updateGameConfigSimpleByKey(key: string, value: string) {
@@ -99,14 +105,39 @@ export default function GameConfig() {
     const newConfig = cloneDeep(gameConfig.value);
     const index = newConfig.findIndex(e => e.command === key);
 
-    if (key === 'Game_menu') {
-      value = value.map((val: any) => `${val?.key}-${val?.show}`)
-    }
-
     if (index >= 0) {
       newConfig[index].args = value;
     } else {
       newConfig.push({command: key, args: value, options: []});
+    }
+
+    gameConfig.set(newConfig);
+    updateGameConfig();
+  }
+
+  function updateGameMenuConfig(options: Record<GameMenuKey, GameMenuItem>) {
+    const newConfig = cloneDeep(gameConfig.value);
+
+    for (const [key, value] of Object.entries(options)) {
+      const index = newConfig.findIndex(e => e.command === key);
+      let styleContent: string[] = [];
+
+      Object.keys((value.args.style)).forEach((key) => {
+        let newKey = key as 'x' | 'y' | 'scale' | 'fontSize' | 'fontColor' | 'image';
+        if (value.args.style[newKey] !== undefined) {
+          styleContent.push(`${key}=${value.args.style[newKey]}`);
+        }
+      });
+
+      const style = styleContent ? `{${styleContent.join(',')}}` : '';
+      const options = [{ key: 'hide', value: value.args.hide }, { key: 'style', value: style }];
+
+      if (index >= 0) {
+        newConfig[index].args = [value.content];
+        newConfig[index].options = options;
+      } else {
+        newConfig.push({command: key, args: [value.content], options });
+      }
     }
 
     gameConfig.set(newConfig);
@@ -181,8 +212,8 @@ export default function GameConfig() {
       <TabItem title={t("options.gameMenu")}>
         <GameConfigEditorGameMenu
           key="gameMenu"
-          value={getConfigGameMenuArray('Game_menu')}
-          onChange={(e: string[]) => updateGameConfigArrayByKey('Game_menu', e)}
+          value={gameConfig.value}
+          onChange={updateGameMenuConfig}
         />
       </TabItem>
     </>
@@ -198,12 +229,6 @@ interface IGameConfigEditor {
 interface IGameConfigEditorMulti {
   key: string;
   value: string[];
-  onChange: Function;
-}
-
-interface IGameConfigEditorMenu {
-  key: string;
-  value: Array<IMenuConfig>;
   onChange: Function;
 }
 
@@ -342,74 +367,262 @@ function GameConfigEditorWithImageFileChoose(props: IGameConfigEditorMulti & {
   );
 }
 
-
-
 /**
  * 游戏菜单控制
  */
-function GameConfigEditorGameMenu(props: IGameConfigEditorMenu) {
-  const t = useTrans("editor.sideBar.gameConfigs.");
-  const defaultGameMenuList: IMenuConfig[] = [
-    { menuName: t('gameMenu.achieve'), key: 'achieve',  show: true },
-    { menuName: t('gameMenu.storyline'), key: 'storyline',  show: true },
-    { menuName: t('gameMenu.beautyGuide'), key: 'beautyGuide',  show: true },
-  ];
-
-  const [menu, setMenu] = useState<IMenuConfig[]>(defaultGameMenuList);
-
-  useEffect(() => {
-
-    if (props.value?.length) {
-      const menuList = props.value.map((item: string | unknown) => {
-        if (typeof item === 'string') {
-          const menukeys: string[] = item?.split('-') ?? [];
-          if (menukeys?.length < 0) return item
-          const menuItem: IMenuConfig | undefined = defaultGameMenuList.find((menu) => menu.key === menukeys[0])
-          if (menuItem) {
-            menuItem['show'] = boolMap.get(menukeys[1]) || false
-            return menuItem
-          }
-          return item
+function GameConfigEditorGameMenu(props: {
+  value: WebgalConfig,
+  onChange: (options: Record<GameMenuKey, GameMenuItem>) => void
+}) {
+  const [gameMenuDialogOpen, setGameMenuDialogOpen] = useState(false);
+  const [options, setOptions] = useState<Record<GameMenuKey, GameMenuItem>>({
+    Game_start_button: {
+      content: "",
+      args: {
+        hide: false,
+        style: {}
+      }
+    },
+    Game_achievement_button: {
+      content: "",
+      args: {
+        hide: false,
+        style: {
         }
+      }
+    },
+    Game_storyline_button: {
+      content: "",
+      args: {
+        hide: false,
+        style: {
+        }
+      }
+    },
+    Game_extra_button: {
+      content: "",
+      args: {
+        hide: false,
+        style: {
+        }
+      }
+    },
+  });
+  const keys = Object.keys(GameMenuKey) as GameMenuKey[];
+  const keysNameMap = {
+    Game_start_button: "开始游戏",
+    Game_achievement_button: "成就",
+    Game_storyline_button: "故事线",
+    Game_extra_button: "鉴赏模式",
+  };
+  
+  useEffect(() => {
+    let GameStartButton = props.value.find(e => e.command === GameMenuKey.Game_start_button);
+    let GameAchievementButton = props.value.find(e => e.command === GameMenuKey.Game_achievement_button);
+    let GameStorylineButton = props.value.find(e => e.command === GameMenuKey.Game_storyline_button);
+    let GameExtraButton = props.value.find(e => e.command === GameMenuKey.Game_extra_button);
 
-        return item
-      }).filter(f => f !== null) as IMenuConfig[]
+    const parseArgs = (args: WebgalConfig[0]['options']) => {
+      const hide = args.find((e: any) => e.key === 'hide')?.value === true;
+      const styleString = (args.find((e: any) => e.key === 'style')?.value as string) || '{}';
+      let styleObj: GameMenuItem['args']['style'] = {};
 
+      const styleRegex = /\{(.*?)\}/;
+      const styleMatch = styleString.match(styleRegex);
+      if (styleMatch) {
+        const styleStr = styleMatch[1];
+        const styleProps = styleStr.split(',');
+        const style: any = {}; // Change to specific type if possible
 
-      if (menuList.length) {
-        setMenu(menuList)
+        // Parse each style property
+        styleProps.forEach((prop) => {
+          const [key, value] = prop.split('=');
+          if (key && value) {
+            style[key.trim()] = isNaN(Number(value.trim())) ? value.trim() : Number(value.trim());
+          }
+        });
+
+        styleObj = style;
       }
 
-    }
+      return {
+        hide,
+        style: styleObj
+      };
+    };
 
-  }, [props.value])
+    setOptions({
+      Game_start_button: {
+        content: GameStartButton?.args[0] ?? '',
+        args: parseArgs(GameStartButton?.options ?? [])
+      },
+      Game_achievement_button: {
+        content: GameAchievementButton?.args[0] ?? '',
+        args: parseArgs(GameAchievementButton?.options ?? [])
+      },
+      Game_storyline_button: {
+        content: GameStorylineButton?.args[0] ?? '',
+        args: parseArgs(GameStorylineButton?.options ?? [])
+      },
+      Game_extra_button: {
+        content: GameExtraButton?.args[0] ?? '',
+        args: parseArgs(GameExtraButton?.options ?? [])
+      },
+    });
+  }, [props.value]);
 
-  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>, key: string) => {
-    const checked = e.target.checked;
-    const list = [...menu];
-    list.forEach((item: IMenuConfig) => {
-      if (item.key === key) {
-        item.show = checked
+  const setStyle = (index: number, styleKey: string, value: number | string | undefined) => {
+    const key = keys[index];
+    setOptions({
+      ...options,
+      [key]: {
+        ...options[key],
+        args: {
+          ...options[key].args,
+          style: {
+            ...options[key].args.style,
+            [styleKey]: value
+          }
+        }
       }
     });
-    setMenu(list);
-    props.onChange(list);
-  }
+  };
+
+  const setHide = (index: number, value: boolean) => {
+    const key = keys[index];
+    setOptions({
+      ...options,
+      [key]: {
+        ...options[key],
+        args: {
+          ...options[key].args,
+          hide: value
+        }
+      }
+    });
+  };
+
+  const setName = (index: number, value: string) => {
+    const key = keys[index];
+    setOptions({
+      ...options,
+      [key]: {
+        ...options[key],
+        content: value
+      }
+    });
+  };
+
+  const submit = () => {
+    setGameMenuDialogOpen(false);
+    setTimeout(() => {
+      props.onChange(options);
+    }, 10);
+  };
 
   return (
-    <>
-      {menu.map((item: IMenuConfig) => {
-        return (
-          <div key={item.key}>
-            <Checkbox 
-              checked={item.show} 
-              onChange={(e) => handleCheckboxChange(e, item.key)}
-              
-            />
-            <span>{item.menuName}</span>
-          </div>
-        )
-      })}
-    </>
-  )
+    <Dialog open={gameMenuDialogOpen} onOpenChange={(event, data) => setGameMenuDialogOpen(data.open)}>
+      <DialogTrigger disableButtonEnhancement>
+        <Button size="small" appearance="primary">
+          菜单设置
+        </Button>
+      </DialogTrigger>
+      <DialogSurface style={{ maxWidth: "840px" }}>
+        <DialogBody>
+          <DialogTitle>标题菜单UI设置</DialogTitle>
+          <DialogContent>
+            {Object.values(options).map((menu, index) => (
+              <div key={index} style={{ marginBlock: "12px" }}>
+                <div style={{ display: "flex", alignItems: "center", marginBottom: "6px" }}>
+                  <span style={{ fontSize: '16px', fontWeight: 500, width: "70px" }}>{(keysNameMap as any)[keys[index]]}</span>
+                  <Checkbox checked={menu.args.hide} onChange={(_, data) => {
+                    console.log(33333,data);
+                    setHide(index, data.checked as boolean);
+                  }} />
+                  <span>隐藏</span>
+                  <span style={{ marginLeft: '38px' }}>按钮名称</span>
+                  <input value={menu.content}
+                    onChange={(ev) => {
+                      setName(index, ev.target.value.trim());
+                    }}
+                    className={styles.sayInput}
+                    placeholder="按钮名称"
+                    style={{ width: "10%", margin: "0 6px 0 12px", color: "#666" }}
+                  />
+                  <span style={{ margin: '0 6px 0 6px' }}>按钮样式 {menu.args.style.image}</span>
+                  <ChooseFile sourceBase="ui" onChange={(newFile) => {
+                    const newValue = newFile?.name ?? "";
+
+                    if (newFile) {
+                      setStyle(index, 'image', newValue);
+                    } else {
+                      setStyle(index, 'image', undefined);
+                    }
+                  }} extName={[".jpg", ".png", "webp"]} />
+                </div>
+                <div style={{  display: "flex", alignItems: "center"}}>
+                  <span style={{ marginLeft: '6px' }}>按钮位置X</span>
+                  <input type="number" value={menu.args.style.x}
+                    onChange={(ev) => {
+                      setStyle(index, 'x', ev.target.value);
+                    }}
+                    className={styles.sayInput}
+                    placeholder="X"
+                    style={{ width: "10%", margin: "0 6px 0 6px" }}
+                  />
+                  <span style={{ marginLeft: '6px' }}>按钮位置Y</span>
+                  <input type="number" value={menu.args.style.y}
+                    onChange={(ev) => {
+                      setStyle(index, 'y', ev.target.value);
+                    }}
+                    className={styles.sayInput}
+                    placeholder="Y"
+                    style={{ width: "10%", margin: "0 6px 0 6px" }}
+                  />
+                  <span style={{ marginLeft: '6px' }}>缩放</span>
+                  <input type="number" value={menu.args.style.scale}
+                    onChange={(ev) => {
+                      setStyle(index, 'scale', ev.target.value);
+                    }}
+                    className={styles.sayInput}
+                    placeholder="缩放"
+                    style={{ width: "10%", margin: "0 6px 0 6px" }}
+                  />
+                  <span style={{ marginLeft: '6px' }}>文字大小</span>
+                  <input type="number" value={menu.args.style.fontSize}
+                    onChange={(ev) => {
+                      setStyle(index, 'fontSize', ev.target.value);
+                    }}
+                    className={styles.sayInput}
+                    placeholder="文字大小"
+                    style={{ width: "10%", margin: "0 6px 0 6px" }}
+                  />
+                  <span style={{ marginLeft: '6px' }}>文字颜色</span>
+                  <input type="color" value={menu.args.style.fontColor || '#fff'}
+                    onChange={(ev) => {
+                      setStyle(index, 'fontColor', ev.target.value);
+                    }}
+                    className={styles.sayInput}
+                    placeholder="文字大小"
+                    style={{ width: "10%", margin: "0 6px 0 6px" }}
+                  />
+                </div>
+              </div>
+            ))}
+          </DialogContent>
+          <DialogActions>
+            <DialogTrigger disableButtonEnhancement>
+              <Button appearance="secondary" onClick={() => {
+                setGameMenuDialogOpen(false);
+              }}>取消</Button>
+            </DialogTrigger>
+            <Button appearance="primary" onClick={() => {
+              submit();
+              setGameMenuDialogOpen(false);
+            }}>确认</Button>
+          </DialogActions>
+        </DialogBody>
+      </DialogSurface>
+    </Dialog>
+  );
 }
