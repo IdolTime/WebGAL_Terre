45fc0fdb1d1f795b1ff1303bce2be08f57f55101
yannannan <18810994501@163.com>
2024-09-04 11:58:22 +0800

    fix: 麻豆sdk bug

--- packages/origine2/src/config/info.ts
+++ packages/origine2/src/config/info.ts
@@ -5,5 +5,5 @@ export interface Info {
 
 export const __INFO: Info = {
   version: '4.4.13',
-  buildTime: '2024-09-03T10:17:22.877Z', // 编译时会通过 version-sync.js 自动更新
+  buildTime: '2024-09-04T03:52:10.857Z', // 编译时会通过 version-sync.js 自动更新
 };
